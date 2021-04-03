5618d9ade8f2751fd1f49d392bd3ac382289983c
dnacher <danielnacher@gmail.com>
2021-04-03 19:56:44 -0300

    se agrega query para traer total de acuerdo a un product entre fechas

--- src/main/java/com/minimarket/minimarketapp/persistence/repository/TransactionLineRepository.java
+++ src/main/java/com/minimarket/minimarketapp/persistence/repository/TransactionLineRepository.java
@@ -1,7 +1,12 @@
 package com.minimarket.minimarketapp.persistence.repository;
 
 import com.minimarket.minimarketapp.persistence.model.TransactionLine;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface TransactionLineRepository extends CrudRepository<TransactionLine, Integer> {
+
+    @Query("SELECT sum(tl.subtotal) from TransactionLine tl where tl=:transactionLine")
+    double sumProductSalesAmount(TransactionLine transactionLine);
+
 }
