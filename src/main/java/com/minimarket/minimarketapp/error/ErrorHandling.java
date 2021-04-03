85703bed9c26eecd86605768a3e0482c5114e12c
dnacher <danielnacher@gmail.com>
2021-04-02 23:14:47 -0300

    clase para manejar errores en exceptions

--- src/main/java/com/minimarket/minimarketapp/error/ErrorHandling.java
+++ src/main/java/com/minimarket/minimarketapp/error/ErrorHandling.java
@@ -4,20 +4,20 @@ import com.minimarket.minimarketapp.persistence.model.Product;
 
 public class ErrorHandling {
 
-    public static String noStockMessage(Product product){
+    public static final String ALREADY_EXIST_PRODUCT_STOCK = "Ya existe stock para este product";
+
+    public static String noStockMessage(Product product) {
         return "No hay stock para el producto " +
                 product.getId() + ": " +
                 product.getDescription();
     }
 
-    public static String TransactionNotFound(String id){
-        return String.format("La transaccion id %s no existe", id);
+    public static String valueNotFound(String value, Integer id) {
+        return String.format("%s con id: %d no existe", value, id);
     }
 
-    public static String TransactionUpdateError(){
-        return "No se puede actualizar una transaccion sin id";
+    public static String valueUpdateError(String value) {
+        return String.format("No se puede actualizar una %s sin id", value);
     }
-
-    
 
 }
