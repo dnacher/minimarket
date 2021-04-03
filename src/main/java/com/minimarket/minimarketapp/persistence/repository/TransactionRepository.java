34f1cf7915956c34f4419d4d6a62a5e4361f4fd5
dnacher <danielnacher@gmail.com>
2021-04-03 19:56:53 -0300

    se agrega query para traer total de acuerdo a un product entre fechas

--- src/main/java/com/minimarket/minimarketapp/persistence/repository/TransactionRepository.java
+++ src/main/java/com/minimarket/minimarketapp/persistence/repository/TransactionRepository.java
@@ -1,9 +1,20 @@
 package com.minimarket.minimarketapp.persistence.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import com.minimarket.minimarketapp.persistence.model.Transaction;
 
+import java.util.Date;
+
 @Repository
 public interface TransactionRepository extends CrudRepository<Transaction, Integer> {
+
+    @Query(value = "SELECT SUM(subtotal) " +
+            "FROM transaction_table " +
+            "JOIN transaction_line on transaction_line.transaction_id=transaction_table.transaction_id " +
+            "WHERE transaction_line.product_id=:productId " +
+            "AND (transaction_table.date BETWEEN :fromDate AND :toDate);", nativeQuery = true)
+    double totalByProductIdBetweenDates(Date fromDate, Date toDate, Integer productId);
+
 }
